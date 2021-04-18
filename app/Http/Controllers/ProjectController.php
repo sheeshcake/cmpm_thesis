69a45d4f68125b205b5309d74ac3f13d95878e47
wendale1231 <spooderm5@gmail.com>
2021-04-18 22:20:05 +0800

    *Update[Misc] **change form to prompt the required fields **making this as a backup **added some feature and changes in time space continuum

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Projects;
 use App\Models\Plans;
 use App\Models\Tasks;
+use App\Models\User;
+
 use Auth;
 
 class ProjectController extends Controller
@@ -15,11 +17,43 @@ class ProjectController extends Controller
     }
 
     public function ShowAddProject(){
+        $users = User::where("role" , "=", "foreman")
+                    ->orWhere("role", "=", "civilengineer")
+                    ->get();
+        $clients = User::where("role", "=", "clie")
         return view("layout." . Auth::user()->role . ".add-project")
                 ->with("data", [
                     "role" => ucfirst(Auth::user()->role),
+                    "users" => $users,
                     "page" => "Add Project"
                     ]);
+    }
+
+    public function ShowProject(Request $request){
+        $project_data = [];
+        $project_details = Projects::where("id", "=", $request->id)->get()->toArray();
+        dd($project_details);
+    }
+
+    public function UpdateProject(Request $request){
+        Project::where("id", "=", $request->project_id)
+                ->update([
+                    "project_name" => $request->data["project_name"],
+                    "project_address" => $request->data["project_address"],
+                    "client_id" => $request->data["client_id"]
+                    ]);
+        Plans::join("tasks", "tasks.plan_id", "=", "plans.plan_id")
+            ->where("plans.project_id", "=", $request->project_id)->delete();
+        foreach($request->data['plans'] as $data){
+            $plans = new Plans();
+            $plans->project_id = $project_id;
+            $plans->plan_name = $data[0];
+            $plans->plan_date_start = $data[1];
+            $plans->plan_date_end = $data[2];
+            $plans->plan_priority = $data[3];
+            $plans->plan_dependency = $data[4];
+            $plans->save();
+        }
     }
 
     public function AddProject(Request $request){
@@ -28,7 +62,29 @@ class ProjectController extends Controller
         $project->project_address = $request->data["project_address"];
         $project->client_id = $request->data["client_id"];
         $project->save();
-        return $request->data;
-
+        $project_id = $project->id;
+        foreach($request->data['plans'] as $plans){
+            $plans = new Plans();
+            $plans->project_id = $project_id;
+            $plans->plan_name = $plans[0];
+            $plans->plan_date_start = $plans[1];
+            $plans->plan_date_end = $plans[2];
+            $plans->plan_priority = $plans[3];
+            $plans->plan_dependency = $plans[4];
+            $plans->save();
+            $plan_id = $plans->id;
+            foreach($request->data["tasks"] as $tasks){
+                $tasks = new Tasks();
+                $tasks->plan_id = $plan_id;
+                $tasks->task_name = $tasks[0];
+                $tasks->task_priority = $tasks[1];
+                $tasks->task_date_start = $tasks[2];
+                $tasks->task_date_end = $tasks[3];
+                $tasks->user_id = $tasks[4];
+                $tasks->save();
+            }
+        }
+        return redirect('projectmanager/project' + $project_id)->with("success", "Project Saved!");
     }
+
 }
