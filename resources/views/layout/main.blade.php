5ae092c13d248dcde6b770a9447f3f490e3293f4
wendale1231 <spooderm5@gmail.com>
2021-05-21 17:02:33 +0800

    update UI

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -2,19 +2,24 @@
 
 @section('content')
         <div class="row justify-content-center">
-            <div class="card mt-3">
+            <div class="card mt-10 border border-warning border-5">
+                <CENTER>
                 <div class="card-header">
-                    <h3>Welcome To</h3>
-                    <h1>Construction Management Performance Monitoring System</h1>
-                    <h6>By: Wendale Dy, Shandy Dominguez</h6>
+                    <h5>WELCOME TO</h5>
+                    <h1 style="letter-spacing: 4px; color: #3784a5;">CMPM System</h1>
+                    <!-- <h7>By: Wendale Dy, Shandy Dominguez</h7> -->
                 </div>
+                </CENTER>
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-4">
+                    <div class="col">
+                        <div>
+                        <center>
+                        <h5>LOGIN AS: </h5>
+                        </center>
                             <div class="list-group" id="list-tab" role="tablist">
-                                <a class="list-group-item list-group-item-action active" id="list-admin-list" data-toggle="list" href="#list-admin" role="tab" aria-controls="admin">Admin</a>
+                                <a class="list-group-item list-group-item-action active" id="list-admin-list" data-toggle="list" href="#list-admin" role="tab" aria-controls="admin">CEO</a>
                                 <a class="list-group-item list-group-item-action" id="list-hr-list" data-toggle="list" href="#list-hr" role="tab" aria-controls="hr">Human Resource</a>
-                                <a class="list-group-item list-group-item-action" id="list-tk-list" data-toggle="list" href="#list-tk" role="tab" aria-controls="tk">Time Keeper</a>
+                                <!-- <a class="list-group-item list-group-item-action" id="list-tk-list" data-toggle="list" href="#list-tk" role="tab" aria-controls="tk">Time Keeper</a> -->
                                 <a class="list-group-item list-group-item-action" id="list-civil-list" data-toggle="list" href="#list-civil" role="tab" aria-controls="civil">Civil Engineer</a>
                                 <a class="list-group-item list-group-item-action" id="list-pm-list" data-toggle="list" href="#list-pm" role="tab" aria-controls="pm">Project Manager</a>
                                 <a class="list-group-item list-group-item-action" id="list-foreman-list" data-toggle="list" href="#list-foreman" role="tab" aria-controls="foreman">Foreman</a>
@@ -22,37 +27,51 @@
                                 <a class="list-group-item list-group-item-action" id="list-client-list" data-toggle="list" href="#list-client" role="tab" aria-controls="client">Client </a>
                             </div>
                         </div>
-                        <div class="col-8">
+                        <div>
                             <div class="tab-content" id="nav-tabContent" >
-                                <div class="tab-pane fade show active" id="list-admin" role="tabpanel" aria-labelledby="list-admin-list">
-                                    <p>Ang admin maka add ug employee like HR, Civil Engineer, Project Manager ug maka kita sa mga sales and shit</p>
+                                <div class="tab-pane fade show active" id="list-admin" role="tabpanel" aria-labelledby="list-admin-list"><br>
+                                    <!-- <p>Ang CEO maka add ug employee like HR, Civil Engineer,</P>
+                                    <P> Project Manager ug maka kita sa mga sales and shit</p> -->
+                                    <div class="d-flex justify-content-center">
                                     <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+                                    </div>
                                 </div>
-                                <div class="tab-pane fade" id="list-hr" role="tabpanel" aria-labelledby="list-hr-list">
-                                    <p>Hr mao ni ang mo add ug mga employee ug mag manage sa payroll</p>
+                                <div class="tab-pane fade" id="list-hr" role="tabpanel" aria-labelledby="list-hr-list"><br>
+                                    <!-- <p>Hr mao ni ang mo add ug mga employee ug mag manage sa payroll</p> -->
+                                    <div class="d-flex justify-content-center">
                                     <a href="{{ route('loginhumanresource') }}" class="btn btn-primary">Login</a>
+                                    </div>
                                 </div>
-                                <div class="tab-pane fade" id="list-tk" role="tabpanel" aria-labelledby="list-hr-list">
+                                <!-- <div class="tab-pane fade" id="list-tk" role="tabpanel" aria-labelledby="list-hr-list">
                                     <p>Time Keeper Mao ni sila ang mo attendance nga absenot nga mga trabahante</p>
-                                </div>
-                                <div class="tab-pane fade" id="list-civil" role="tabpanel" aria-labelledby="list-civil-list">
-                                    <p>Civil Engineer kay mao na ang mo monitor sa project</p>
+                                </div> -->
+                                <div class="tab-pane fade" id="list-civil" role="tabpanel" aria-labelledby="list-civil-list"><br>
+                                    <!-- <p>Civil Engineer kay mao na ang mo monitor sa project</p> -->
+                                    <div class="d-flex justify-content-center">
                                     <a href="{{ route('logincivilengineer') }}" class="btn btn-primary">Login</a>
+                                    </div>
                                 </div>
-                                <div class="tab-pane fade" id="list-pm" role="tabpanel" aria-labelledby="list-pm-list">
-                                    <p>Project Manager mao ni ang mo plano sa project ug mag manage sa time frame</p>
+                                <div class="tab-pane fade" id="list-pm" role="tabpanel" aria-labelledby="list-pm-list"><br>
+                                    <!-- <p>Project Manager mao ni ang mo plano sa project ug mag manage sa time frame</p> -->
+                                    <div class="d-flex justify-content-center">
                                     <a href="{{ route('loginprojectmanager') }}" class="btn btn-primary">Login</a>
+                                    </div>
                                 </div>
-                                <div class="tab-pane fade" id="list-foreman" role="tabpanel" aria-labelledby="list-foreman-list">
-                                    <p>Foreman Mao ni sila ang hatagan always sa task tas sila na bahala mo sugo sa mga laborers, mason, etc..</p>
+                                <div class="tab-pane fade" id="list-foreman" role="tabpanel" aria-labelledby="list-foreman-list"><br>
+                                    <!-- <p>Foreman Mao ni sila ang hatagan always sa task tas sila </p>
+                                    <p>na bahala mo sugo sa mga laborers, mason, etc..</p> -->
+                                    <div class="d-flex justify-content-center">
                                     <a href="{{ route('loginforeman') }}" class="btn btn-primary">Login</a>
+                                    </div>
                                 </div>
-                                <div class="tab-pane fade" id="list-expediter" role="tabpanel" aria-labelledby="list-expediter-list">
-                                    <p>Expediter mao ni ang mo manage sa mga supplies</p>
+                                <div class="tab-pane fade" id="list-expediter" role="tabpanel" aria-labelledby="list-expediter-list"><br>
+                                    <!-- <p>Expediter mao ni ang mo manage sa mga supplies</p> -->
+                                    <div class="d-flex justify-content-center">
                                     <a href="{{ route('loginexpediter') }}" class="btn btn-primary">Login</a>
+                                    </div>
                                 </div>
                                 <div class="tab-pane fade" id="list-client" role="tabpanel" aria-labelledby="list-expediter-list">
-                                    <p>Client Mao ni sila ang moreklamo ug maka kita sa progress sa ilang balay litsi!</p>
+                                    <!-- <p>Client Mao ni sila ang moreklamo ug maka kita sa progress sa ilang balay</p> -->
                                 </div>
                             </div>
                         </div>
